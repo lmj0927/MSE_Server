f413e6ffd028f6c2a3ee6e8cec40d5cd09a6a02b
MinJunLee <lmjj0927@naver.com>
2026-05-15 01:39:24 +0900

    Init Spring

---
+++ src/main/java/kc/ar/ajou/mseserver/MseserverApplication.java
@@ -0,0 +1,13 @@
+package kc.ar.ajou.mseserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MseserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MseserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kc/ar/ajou/mseserver/MseserverApplicationTests.java
@@ -0,0 +1,13 @@
+package kc.ar.ajou.mseserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MseserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
